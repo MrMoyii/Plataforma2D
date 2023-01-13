49d08d6766a557a989c765d96bf5b1fd9f54b5d1
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-13 13:21:39 -0300

    Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,26 +10,32 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D Rigidbody2D;
     private float horizontal;
     private bool grounded;
+    private Animator animator;
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //obtenener el valor de la direccion de movimiento
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        //Animacion cuando se mueve
+        animator.SetBool("running", horizontal != 0.0f);
+
+        //dibuja un rayo
         Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red);
-        if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f))
-        {
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
-        }
+
+        //Lanza un rayo 
+        grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f) ? true : false;
+        //if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f))
+        //    grounded = true;
+        //else
+        //    grounded = false;
 
         if (Input.GetKeyDown(KeyCode.W) && grounded)
         {
